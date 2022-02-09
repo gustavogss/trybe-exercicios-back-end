0e18b607d6ef71970b6518c33363377e4fc8d780
Gustavo Souza <gustavogss.dev@gmail.com>
2022-02-09 10:07:09 -0300

    Bloco 22 - Exercicios Requisito 5

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/Exercicio22.1/my-scripts/imc.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/Exercicio22.1/my-scripts/imc.js
@@ -1,13 +1,32 @@
-const readline = require('readline-sync');
+const readline = require("readline-sync");
 
-const altura = readline.questionFloat('Qual é a sua altura: ');
-const peso = readline.questionFloat('Qual é o seu peso: ');
-
+const altura = readline.questionFloat("Qual é a sua altura: ");
+const peso = readline.questionFloat("Qual é o seu peso:");
 
 const imc = peso / Math.pow(altura, 2).toFixed(2);
 
-console.log(`Seu IMC é ${imc}`)
+switch (imc){
+  case (imc < 18.5):
+    console.log(`Seu IMC é ${imc} - Você está abaixo do peso (magreza)`);
+    break;
+  case (18.5 > imc && imc < 24.9):
+    console.log(`Seu IMC é ${imc} - Você está no peso normal`);
+    break;
+  case (25.0 > imc && imc  < 29.9):
+    console.log(`Seu IMC é ${imc} - Você está acima do peso (sobrepeso)`);
+    break;
+  case (30.0 > imc && imc  < 34.9):
+    console.log(`Seu IMC é ${imc} - Você está com Obesidade grau I`);
+    break;
+  case (35.0 > imc && imc  < 39.9):
+    console.log(`Seu IMC é ${imc} - Você está com Obesidade grau II`);
+    break;
+  default:
+    console.log(`Seu IMC é ${imc} - Você está com Obesidade graus III e IV`);
+}
 
 module.export = {
-  altura, peso, imc
-} 
+  altura,
+  peso,
+  imc,
+};
